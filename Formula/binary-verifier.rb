1b3a19930221b7c3fff914259698ba2c09de1874
perrythomas <thomasperryjr@perrythomass-MacBook-Pro.local>
2025-09-09 19:09:46 -0500

    Update formula with correct URL and SHA256

--- Formula/binary-verifier.rb
+++ Formula/binary-verifier.rb
@@ -3,7 +3,7 @@ class BinaryVerifier < Formula
   homepage "https://github.com/perryjr1444-ux/binary-verifier"
   url "https://github.com/perryjr1444-ux/binary-verifier/archive/refs/heads/main.zip"
   version "1.0.0"
-  sha256 "" # This will be filled in after we create the GitHub repo
+  sha256 "c4896c236b1d090130d9f045a1eb3e8c84870b1317cd1ac84da6d9e8d034e9e2" # This will be filled in after we create the GitHub repo
   license "MIT"
 
   def install
